797982d689618e5aa380a7b2e7cc4bcb7239b4cc
BINAYAK BIDYASAGAR <binayak@profecialinks.com>
2022-01-30 20:08:21 +0530

    training of html started

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 // var React = require('react');
+
+//build component
+const App = () => {
+    return(
+        <h1>hi from react</h1>
+    )
+}
+ReactDom.render(<App/>, document.getElementById('root'))
